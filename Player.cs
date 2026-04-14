58ee4ba14408c4156cbfb1d498e386632eec5374
konslee159 <lywman123@gmail.com>
2026-04-14 23:01:20 +0900

    첫 번째 커밋

--- Player.cs
+++ Player.cs
@@ -4,54 +4,34 @@ using System;
 public partial class Player : Area2D
 {
 	[Export]
-	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
-	
+	public int Speed { get; set; } = 400;
+
 	[Signal]
 	public delegate void HitEventHandler();
 
-	public Vector2 ScreenSize; // Size of the game window.
-	
+	public Vector2 ScreenSize;
+
 	public void Start(Vector2 position)
 	{
 		Position = position;
 		Show();
 		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
 	}
-	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready()
 	{
-		Hide(); // Player disappears after being hit.
 		ScreenSize = GetViewportRect().Size;
-		
-		EmitSignal(SignalName.Hit);
-		// Must be deferred as we can't change physics properties on a physics callback.
-		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+		Hide(); // 게임 시작 시 숨기기
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		 var velocity = Vector2.Zero; // The player's movement vector.
+		var velocity = Vector2.Zero;
 
-		if (Input.IsActionPressed("move_right"))
-		{
-			velocity.X += 1;
-		}
-
-		if (Input.IsActionPressed("move_left"))
-		{
-			velocity.X -= 1;
-		}
-
-		if (Input.IsActionPressed("move_down"))
-		{
-			velocity.Y += 1;
-		}
-
-		if (Input.IsActionPressed("move_up"))
-		{
-			velocity.Y -= 1;
-		}
+		if (Input.IsActionPressed("move_right")) velocity.X += 1;
+		if (Input.IsActionPressed("move_left"))  velocity.X -= 1;
+		if (Input.IsActionPressed("move_down"))  velocity.Y += 1;
+		if (Input.IsActionPressed("move_up"))    velocity.Y -= 1;
 
 		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
@@ -64,17 +44,17 @@ public partial class Player : Area2D
 		{
 			animatedSprite2D.Stop();
 		}
-		
+
 		Position += velocity * (float)delta;
 		Position = new Vector2(
-		x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-		y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
 		);
+
 		if (velocity.X != 0)
 		{
 			animatedSprite2D.Animation = "walk";
 			animatedSprite2D.FlipV = false;
-			// See the note below about the following boolean assignment.
 			animatedSprite2D.FlipH = velocity.X < 0;
 		}
 		else if (velocity.Y != 0)
@@ -82,5 +62,13 @@ public partial class Player : Area2D
 			animatedSprite2D.Animation = "up";
 			animatedSprite2D.FlipV = velocity.Y > 0;
 		}
+	}
+
+	// 적이 닿았을 때
+	private void OnBodyEntered(Node2D body)
+	{
+		Hide();
+		EmitSignal("Hit");
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
 	}
 }
